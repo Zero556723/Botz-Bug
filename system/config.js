92b87aa8da69894891177d6a5259d7708422c8ed
@Revv404 <a8631720@gmail.com>
2025-07-18 15:22:12 +0700

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,15 @@
+//===========================//
+global.prefa = ["","!",".",",","🐤","🗿"]
+global.owner = ["62xxx"]
+global.NamaOwner = "By.Thonxyzz404"
+global.namabot = "BOTZ~BUGS"
+//===========================//
+//Global Mess
+global.mess = {
+ ingroup: "It's not funny, this feature is only for groups💢",
+ admin: "not funny, only group admins use this feature💢",
+ owner: "Wow! You're not my owner🗣️",
+ premium: "you are not a premium user",
+ success: 'Success bro✅',
+}
+//===========================//
